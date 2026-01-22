8b1f0f12ecbc35eb56b36b6167885e255d532fea
thedineshjoshi <9868459709dinesh@gmail.com>
2026-01-22 22:08:14 +0545

    Project made ready for migration

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DbString"));
+            });
+            services.AddScoped<IApplicationDbContext>(provider=>provider.GetService<ApplicationDbContext>());
             return services;
         }
 
--- SMART_WARD_MANAGEMENT_SYSTEM/Program.cs
+++ SMART_WARD_MANAGEMENT_SYSTEM/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddSmartWardManagementSystemDI();
+builder.Services.AddSmartWardManagementSystemDI(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
