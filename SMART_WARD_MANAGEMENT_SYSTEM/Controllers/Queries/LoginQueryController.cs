da950e48c48b4b213194058a7df65040303ae1fc
thedineshjoshi <9868459709dinesh@gmail.com>
2026-01-31 22:29:35 +0545

    LoginQueryController implemented

---
+++ SMART_WARD_MANAGEMENT_SYSTEM/Controllers/Queries/LoginQueryController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Login.Query;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMART_WARD_MANAGEMENT_SYSTEM.Controllers.Queries
+{
+    public class LoginQueryController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+        public LoginQueryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginQuery query)
+        {
+            var token = await _mediator.Send(query);
+            return Ok(token);
+        }
+
+    }
+}
