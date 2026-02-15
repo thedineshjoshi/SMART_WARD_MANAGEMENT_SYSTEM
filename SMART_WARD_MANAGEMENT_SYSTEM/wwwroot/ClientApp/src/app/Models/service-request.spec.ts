bcc64e4fb0b4b2e3fa8037099224d98c4ba47361
Anil Chandra Giri <123819979+Anil-Chandra-Giri@users.noreply.github.com>
2026-02-15 11:37:46 +0545

    added API call services

---
+++ SMART_WARD_MANAGEMENT_SYSTEM/wwwroot/ClientApp/src/app/Models/service-request.spec.ts
@@ -0,0 +1,7 @@
+import { ServiceRequest } from './service-request';
+
+describe('ServiceRequest', () => {
+  it('should create an instance', () => {
+    expect(new ServiceRequest()).toBeTruthy();
+  });
+});
